3b8bd1538454d25fdb7986e28cb2dee09e172cd3
FabienMarti <martiretti.fabien@gmail.com>
2020-06-19 06:48:19 +0200

    premier envoie CV numérique

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- Inclusion du header avec PHP -->
+<?php include('view/parts/header.php'); ?>
+<main class="container">
+  <h1 class="text-center mt-5">A PROPOS DE <span class="greenWord">MOI</span></h1>
+  <h2 class="text-center">Introduction brève sur moi-même</h2>
+  <p class="text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+  <div class="row mt-5">
+    <div class="col-5 col-md-4">
+      <img class="img-fluid m-auto" src="assets/img/moi.jpg" alt="Photographie du candidat" title="MARTIRETTI Fabien" />
+    </div>
+    <div class="col-md-8">
+      <h3 class="greenWord">MARTIRETTI FABIEN</h3>
+      <h4 class="mb-3">Codeur - Développeur Web et Web mobile</h4>
+      <p>Hoc inmaturo interitu ipse quoque sui pertaesus excessit e vita aetatis nono anno atque vicensimo cum quadriennio imperasset. natus apud Tuscos in Massa Veternensi, patre Constantio Constantini fratre imperatoris, matreque Galla sorore Rufini et Cerealis, quos trabeae consulares nobilitarunt et praefecturae.</p>
+      <p>Illud tamen te esse admonitum volo, primum ut qualis es talem te esse omnes existiment ut, quantum a rerum turpitudine abes, tantum te a verborum libertate seiungas; deinde ut ea in alterum ne dicas, quae cum tibi falso responsa sint, erubescas. Quis est enim, cui via ista non pateat, qui isti aetati atque etiam isti dignitati non possit quam velit petulanter, etiamsi sine ulla suspicione, at non sine argumento male dicere? Sed istarum partium culpa est eorum, qui te agere voluerunt; laus pudoris tui, quod ea te invitum dicere videbamus, ingenii, quod ornate politeque dixisti.</p>
+      <button class="btn">Télécharger résumé</button>
+    </div>
+  </div>
+</main>
+<!-- Inclusion du footer avec PHP -->
+<?php include('view/parts/footer.php'); ?>
---
+++ view/parts/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <!-- Viewport pour responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    <!-- BootsWatch Sandstone -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <!-- Menu de Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" id="name" href="#">FABIEN <span class="greenWord">MARTIRETTI</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="skills.html">Compétences</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Expériences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li>
+              </ul>
+            </div>
+          </nav>
+          <div id="headerBottomBG"></div>
+    </header>
